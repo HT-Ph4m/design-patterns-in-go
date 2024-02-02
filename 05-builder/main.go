fa6829fd825eccd05ab6db988fea500c5ad364d7
Groot <hongtruongpham18@gmail.com>
2024-02-02 15:45:28 +0700

    builder pattern

---
+++ 05-builder/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"golang/pattern/05-builder/builder"
+)
+
+func main() {
+	normalBuilder := builder.GetBuilder("normal")
+	iglooBuilder := builder.GetBuilder("igloo")
+
+	director := builder.NewDirector(normalBuilder)
+	normalHouse := director.BuildHouse()
+
+	fmt.Printf("Normal House Door Type: %s\n", normalHouse.GetDoorType())
+	fmt.Printf("Normal House Window Type: %s\n", normalHouse.GetWindowType())
+	fmt.Printf("Normal House Num Floor : %d\n", normalHouse.GetFloor())
+
+	director2 := builder.NewDirector(iglooBuilder)
+	iglooHouse := director2.BuildHouse()
+
+	fmt.Println()
+	fmt.Printf("Normal House Door Type: %s\n", iglooHouse.GetDoorType())
+	fmt.Printf("Normal House Window Type: %s\n", iglooHouse.GetWindowType())
+	fmt.Printf("Normal House Num Floor : %d\n", iglooHouse.GetFloor())
+}
